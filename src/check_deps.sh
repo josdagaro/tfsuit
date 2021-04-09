7688897cc832f91de230794107968aa9e97bda93
José David García Rodríguez <josdagaro@gmail.com>
2021-04-09 18:54:03 -0500

    Change "if way"

--- src/check_deps.sh
+++ src/check_deps.sh
@@ -8,4 +8,6 @@ if ! command -v gsht &>/dev/null; then
   die "The command gsht is not installed"
 fi
 
-[ ! -f "$config_json_path" ] && die "The configuration JSON file ${config_json_path} does not exist"
+if [ ! -f "$config_json_path" ]; then
+  die "The configuration JSON file ${config_json_path} does not exist"
+fi
