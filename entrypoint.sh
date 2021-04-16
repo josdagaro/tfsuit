c2ed16425dd455321ef00c526b330dd6815d9b96
José David García Rodríguez <josdagaro@gmail.com>
2021-04-15 19:12:36 -0500

    Launch tfsuit from Docker container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+set -eu
+
+dir_flag=
+config_json_path_flag=
+fail_on_not_compliant_flag=
+
 # Actions pass inputs as $INPUT_<input name> environmet variables
-[[ -n "$INPUT_DIR" ]] && dir_flag="-dir=${INPUT_DIR}"
+[[ -n "$INPUT_DIR" ]] && dir_flag="-d=${INPUT_DIR}"
 [[ -n "$INPUT_CONFIG_JSON_PATH" ]] && config_json_path_flag="-c=${INPUT_CONFIG_JSON_PATH}"
 [[ -n "$INPUT_FAIL_ON_NOT_COMPLIANT" ]] && fail_on_not_compliant_flag="-f"
 
-echo "flags:
+echo "inputs:
   ${dir_flag}
   ${config_json_path_flag}
   ${fail_on_not_compliant_flag}
 "
+
+tfsuit "$dir_flag" "$config_json_path_flag" "$fail_on_not_compliant_flag"
+tfsuit_exit_code=$?
+exit "$tfsuit_exit_code"
