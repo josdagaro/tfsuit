a38d472c6637d03e77a266b394b56134a5497f55
José David García Rodríguez <josdagaro@gmail.com>
2022-05-04 20:34:41 -0500

    Add some comments for improving the understanding of the project and add support for analysing TF modules name conventions

--- src/tfsuit.sh
+++ src/tfsuit.sh
@@ -11,14 +11,21 @@ tfsuit() {
     source check_deps.sh
     source evaluator.sh
 
+    # Initialization of variables for TF variables
     local compliant_vars
     local not_compliant_vars
     local vars_sum
     local vars_message
+    # Initialization of variables for TF outputs
     local compliant_outputs
     local not_compliant_outputs
     local outputs_sum
     local outputs_message
+    # Initialization of variables for TF modules
+    local compliant_mods
+    local not_compliant_mods
+    local mods_sum
+    local mods_message
     local error_exists
     local message
     vars_message=""
@@ -55,9 +62,20 @@ tfsuit() {
       error_exists=1
     fi
 
+    mods_sum=$(evaluator::eval --context="modules" --context-full-name="module" --obj-naming-convention-match-pattern-beginning="module\s+" --obj-match-pattern-1='^(?!#*$)([\s]+)?module\s+([a-z0-9_]+|"[a-z0-9_]+")' --obj-match-pattern-2='module\s+([a-z0-9_]+|"[a-z0-9_]+")')
+    compliant_mods=$(echo "$mods_sum" | jq -r .compliant)
+    not_compliant_mods=$(echo "$mods_sum" | jq -r .not_compliant)
+    echo "compliant modules:"
+    echo "$compliant_mods" | jq
+    echo "::set-output name=compliant_modules::$(echo "$compliant_mods" | jq -rc)"
+    echo "not compliant modules:"
+    echo "$not_compliant_mods" | jq
+    echo "::set-output name=not_compliant_modules::$(echo "$not_compliant_mods" | jq -rc)"
+
     message+="
       $vars_message
       $outputs_message
+      $mods_message
     "
 
     if [ "$error_exists" -eq 1 ] && [ "$fail_on_not_compliant" -eq 1 ]; then
---
+++ tests/modules.tf
@@ -0,0 +1,62 @@
+module "ecs_foobar" {
+    source = "../"
+
+    foo = "bar"
+    bar = "foo"
+}
+
+    module "ecs_barfoo" {
+    source = "../"
+
+    foo = "bar"
+    bar = "foo"
+}
+
+    module "ecs_foo" {
+        source = "../"
+
+        foo = "bar"
+        bar = "foo"
+    }
+
+module "ecs_bar" {
+source = "../"
+
+foo = "bar"
+bar = "foo"
+}
+
+module "ecs_bar_virginia" {
+source = "../"
+
+foo = "bar"
+bar = "foo"
+}
+
+                    module "ecs_foo_virginia" {
+source = "../"
+
+foo = "bar"
+bar = "foo"
+}
+
+module "virginia" {
+    source = "../"
+
+    foo = "bar"
+    bar = "foo"
+}
+
+module ecs_virginia {
+    source = "../"
+
+    foo = "bar"
+    bar = "foo"
+}
+
+module ecs_bad {
+    source = "../"
+
+    foo = "bar"
+    bar = "foo"
+}
